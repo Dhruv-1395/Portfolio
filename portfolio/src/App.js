479f83ec8442a8b78c2a1eceffe1cd4dd82a5e96
Dhruv-1395 <official.dhruv1395@gmail.com>
2024-12-02 12:31:40 +0530

    add navbar and home page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,11 @@
-
+import Navbar from "./componant/Navbar";
+import './App.css'
+import Home from "./componant/Home";
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-[#212428] h-auto w-100 ">
+      <Navbar />
+      <Home />
     </div>
   );
 }
